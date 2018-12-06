e62f7f5eeed64a5606a8a5ba33aab9cf6ff825d9
Nikola Kavelj <Nikola Kavelj@KAVELJ-DESKTOP>
2018-12-06 22:57:46 +0100

    New commit.

--- Aufgabe1.m
+++ Aufgabe1.m
@@ -2,27 +2,27 @@
 oe = [Oertlich(1:9,2),Oertlich(1:9,1)];
 gk = [GK(:,2),GK(:,1)];
 %% Parameter, P1 und P5
-dx1=gk(5,1)-gk(1,1);
-dx_1=oe(5,1)-oe(1,1);
-dy1=gk(5,2)-gk(1,2);
-dy_1=oe(5,2)-oe(1,2);
+dx1=oe(5,1)-oe(1,1);
+dx_1=gk(5,1)-gk(1,1);
+dy1=oe(5,2)-oe(1,2);
+dy_1=gk(5,2)-gk(1,2);
 a1=(dx1*dx_1+dy1*dy_1)/(dx1^2+dy1^2);
 o1=(dx1*dy_1+dy1*dx_1)/(dx1^2+dy1^2);
 disp('Parametersatz, P1 und P5');
 x0_1=gk(1,1)-a1*oe(1,1)+o1*oe(1,2)
-y0_1=gk(1,1)-o1*oe(1,1)-a1*oe(1,2)
+y0_1=gk(1,2)-o1*oe(1,1)-a1*oe(1,2)
 m1=sqrt(o1^2+a1^2)
 e1=atan2(o1,a1)
 %% Parameter, P1 und P9
-dx2=gk(9,1)-gk(1,1);
-dx_2=oe(9,1)-oe(1,1);
-dy2=gk(9,2)-gk(1,2);
-dy_2=oe(9,2)-oe(1,2);
+dx2=oe(9,1)-oe(1,1);
+dx_2=gk(9,1)-gk(1,1);
+dy2=oe(9,2)-oe(1,2);
+dy_2=gk(9,2)-gk(1,2);
 a2=(dx2*dx_2+dy2*dy_2)/(dx2^2+dy2^2);
 o2=(dx2*dy_2+dy2*dx_2)/(dx2^2+dy2^2);
 disp('Parametersatz, P1 und P9');
 x0_2=gk(1,1)-a2*oe(1,1)+o2*oe(1,2)
-y0_2=gk(1,1)-o2*oe(1,1)-a2*oe(1,2)
+y0_2=gk(1,2)-o2*oe(1,1)-a2*oe(1,2)
 m2=sqrt(o2^2+a2^2)
 e2=atan2(o2,a2)
 %% Transformation
